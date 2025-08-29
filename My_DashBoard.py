9a819479e366660b798b8237fb8ce9dbd164c935
springgone72-byte <223993158+springgone72-byte@users.noreply.github.com>
2025-08-29 17:00:42 +0900

    Add files via upload

---
+++ My_DashBoard.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import altair as alt
+import plotly.express as px
+
+box = st.container(border=True)          # 테두리 있는 컨테이너 (>= 1.28)
+box.markdown('''
+    # **:green[Turn-Around-Time]** **:red[Trend]**''')
+
+st.markdown('Y2025 Monthly Increase/Decrease Status')
+col1, col2 = st.columns(2)
+
+with col1:
+    st.text('Line Chart')
+    chart_data = pd.read_csv('TAT.csv')
+    st.line_chart(chart_data, use_container_width=True)
+
+with col2:
+    st.text('Bar Chart')
+    chart_data = pd.read_csv('TAT.csv')
+    st.bar_chart(chart_data)
+
+
