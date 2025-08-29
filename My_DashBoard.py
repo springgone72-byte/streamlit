5c56faf1060b3613638595701b9a1591b0828ada
springgone72-byte <223993158+springgone72-byte@users.noreply.github.com>
2025-08-29 17:18:31 +0900

    Update My_DashBoard.py

--- My_DashBoard.py
+++ My_DashBoard.py
@@ -6,7 +6,7 @@ import altair as alt
 import plotly.express as px
 
 box = st.container(border=True)          
-box.markdown(# **:green[Turn-Around-Time]** **:red[Trend]**)
+box.markdown('# **:green[Turn-Around-Time]** **:red[Trend]**')
 
 st.markdown('Y2025 Monthly Increase/Decrease Status')
 col1, col2 = st.columns(2)
@@ -20,6 +20,7 @@ with col2:
     st.text('Bar Chart')
     chart_data = pd.read_csv('TAT.csv')
     st.bar_chart(chart_data)
+
 
 
 
